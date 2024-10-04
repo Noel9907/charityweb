21b95115b9a10e1c46439b53402091ec3035723a
Noel <noelskocheekkaran@gmail.com>
2024-10-04 11:54:43 +0530

    final

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,38 +1,16 @@
 require("@matterlabs/hardhat-zksync-solc");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
-  networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://rpc.ankr.com/eth_sepolia",
+        accounts: `0x6ef0C798e78081DBB3e8F474f454E72F7A8543Be`,
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
